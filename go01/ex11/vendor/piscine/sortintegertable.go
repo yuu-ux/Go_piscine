4637a4c42b022a6796cc0eab565b55752e53b1a5
yuu-ux <yuugaehara@gmail.com>
2024-06-28 20:06:40 +0900

    feat: add ex11

--- go01/ex11/vendor/piscine/sortintegertable.go
+++ go01/ex11/vendor/piscine/sortintegertable.go
@@ -1,4 +1,19 @@
-package piscine 
+package piscine
 
+func count(array []int) int {
+	var len int = 0
+	for range array {
+		len++
+	}
+	return len
+}
 func SortIntegerTable(table []int) {
+	var length = count(table)
+	for i := 0; i < length - 1; i++ {
+		for j := length - 1; i < j; j-- {
+			if table[j-1] > table[j] {
+				table[j-1], table[j] = table[j], table[j-1]
+			}
+		}
+	}
 }

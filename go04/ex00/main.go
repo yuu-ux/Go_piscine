ccf0819fd700ebb081510a767dc859833a4906e1
Yuga Ehara <yehara@c1r7s1.42tokyo.jp>
2024-07-07 13:29:10 +0900

    feat: modify ex00 in go04

--- go04/ex00/main.go
+++ go04/ex00/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "os"
+import (
+	"os"
+	"ft"
+)
 
-func main() {
-	println(os.Args(0))
+func findLastsrash(s string) int {
+	indexSrash := 0
+	for i, r := range s {
+		if r == '/' {
+			indexSrash = i + 1
+		}
+	}
+	return indexSrash
 }
 
+func main() {
+	indexSrash := findLastsrash(os.Args[0])
+	os.Stdout.Write([]byte(os.Args[0][indexSrash:]))
+	ft.PrintRune('\n')
+}

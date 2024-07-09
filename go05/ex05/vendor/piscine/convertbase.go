5bee7a98870bcd5ef1f94005982901bc4e45901a
Yuga Ehara <yehara@c1r1s8.42tokyo.jp>
2024-07-09 22:21:22 +0900

    feat: modify ex05/vendor/piscine/convertbase.go

--- go05/ex05/vendor/piscine/convertbase.go
+++ go05/ex05/vendor/piscine/convertbase.go
@@ -1,5 +1,58 @@
 package piscine
 
-func ConvertBase(nbr, baseFrom, baseTo string) string {
+func CountDigit(base string) int {
+    var digit int
+    for range base {
+        digit++
+    }
+    return digit
+}
 
+func ReturnNbr(nbr int, digit int, base []rune, result string) string {
+    if nbr != 0 {
+        result = ReturnNbr(nbr / digit, digit, base, result)
+        result += string(base[nbr % digit])
+    }
+	return result
+}
+
+func SearchIndex (c rune, base string) int {
+    for i, c1 := range base {
+        if c == c1 {
+            return i
+        }
+    }
+    return 0
+}
+
+func AtoiBase(s string, base string) int {
+    var digit = CountDigit(base)
+    var result int
+    var index int
+
+    for _, c := range s {
+        if !(c >= '0' && c <= '9') {
+            index = SearchIndex(c, base)
+        } else {
+            index = (int(c) - '0')
+		}
+        result = (result * digit) + index
+    }
+    return result
+}
+
+func PrintNbrBase(nbr int, base string) string {
+    var digit = CountDigit(base)
+	var result string
+
+    if nbr < 0 {
+        nbr *= -1
+    }
+    return ReturnNbr(nbr, digit, []rune(base), result)
+}
+
+func ConvertBase(nbr, baseFrom, baseTo string) string {
+	decimal := AtoiBase(nbr, baseFrom)
+	result := PrintNbrBase(decimal, baseTo)
+	return result
 }
